3902343f0f613ab0fbc8bf545805adfc9c16fe7e
Milito97 <90428595+Milito97@users.noreply.github.com>
2022-09-20 23:36:06 -0600

    added getters and setters

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,24 @@
 
 public class Employee {
 
+	int empNo;
+	String empName;
+	String department;
+	char type;
+	double payRate;
+	double maxHours;
+	
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
 }
